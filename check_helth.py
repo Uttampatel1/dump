a7fd246e09fe2967e8c01583dc2cde0e5acdaf5c
Uttam patel <utampipaliya@gmail.com>
2022-09-21 18:38:39 +0530

    new line

--- check_helth.py
+++ check_helth.py
@@ -1,7 +1,7 @@
 # The script is pretty simple. It checks for a reboot, disk full, and root partition full. If any of these checks fail, the script will exit with a non-zero exit code. This will cause the script to fail and the user will be notified.
 
 # The script is run by the cron job every 5 minutes. The cron job is set up by the following line in the crontab file:
-
+# this is new line
 # */5 * * * * /usr/bin/python3 /home/automation/check_health.py
 
 # The script is run every 5 minutes and the output is sent to the user’s email address.
